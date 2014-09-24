027fa1722531824483d8f35d978965525933f21b
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2014-09-24 09:56:09 -0400

    Finished checkpoint. Committing before working on assignment

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Ricardo Gonzalez on 9/24/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
